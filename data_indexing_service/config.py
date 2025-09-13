6c24225d2b2c4492c23ef925d4a420a509c67cc2
poojasree-kc <poojasree@kittycash.dev>
2025-09-13 18:54:52 +0000

    updates

--- data_indexing_service/config.py
+++ data_indexing_service/config.py
@@ -2,3 +2,5 @@ EMBED_MODEL = "BAAI/bge-m3"
 INDEX_DIR = "../data/faiss_index"
 DOCSTORE_PATH = "../data/docstore.json"
 KB_PATH = "../data/knowledge_base.txt"
+KB_FILES_DIR = "../data/kb_files"
+
--- data_indexing_service/embedder.py
+++ data_indexing_service/embedder.py
@@ -11,6 +11,3 @@ class Embedder:
             convert_to_numpy=True,
             show_progress_bar=True,
         )
-
-
-
